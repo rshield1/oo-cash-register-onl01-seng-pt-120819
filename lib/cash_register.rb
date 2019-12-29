400f7d42edd38f291a403024720b600bcba8ec97
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-29 20:47:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
-
+class CashRegister
+  
+attr_accessor :items, :discount, :total, :last_transaction
+  
+    def initialize(discount=0)
+      @total = 0
+      @discount = discount
+      @items = []
+    end
+  
+  def add_item(title, price, quantity = 1)
+  self.total += price * quantity
+      quantity.times { items << title }
+      self.last_transaction = price * quantity
+  end
+  
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      self.total = @total - @discount
+    end
+  end
+  
+  def void_last_transaction
+    self.total = @total - self.last_transaction
+      if self.total == 0.0 || 0 
+        @total
+      end
+    end
+  
+end
